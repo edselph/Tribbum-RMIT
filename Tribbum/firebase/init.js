3fc32295f62ca4325a8943329c0ad9b001bc057a
Sleepy <sensen.edsel@gmail.com>
2024-05-02 16:36:49 +1000

    groups display
    add groups display button n a new page for displaying the groups!

--- Tribbum/firebase/init.js
+++ Tribbum/firebase/init.js
@@ -1,18 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
-<<<<<<< Updated upstream
-import { connectStorageEmulator, getStorage } from "firebase/storage";
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-import { connectStorageEmulator, getStorage } from "firebase/storage";
->>>>>>> fcb35e205345d069d3a0c413ccb93a4cdb9d2b60
-=======
-import { connectStorageEmulator, getStorage } from "firebase/storage";
->>>>>>> 92204dfd913a2c855d40c941b8a51efb051042a7
->>>>>>> Stashed changes
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -33,26 +21,10 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firestore
 const db = getFirestore(app);
-const storage = getStorage(app);
 
 // Connect to Firestore Emulator in development
 if (process.env.NODE_ENV === "development") {
-<<<<<<< Updated upstream
   connectFirestoreEmulator(db, "127.0.0.1", 8080);
-  connectStorageEmulator(storage, "127.0.0.1", 9199);
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-  connectFirestoreEmulator(db, "localhost", 8080);
-=======
-  connectFirestoreEmulator(db, "127.0.0.1", 8080);
-  connectStorageEmulator(storage, "127.0.0.1", 9199);
->>>>>>> fcb35e205345d069d3a0c413ccb93a4cdb9d2b60
-=======
-  connectFirestoreEmulator(db, "127.0.0.1", 8080);
-  connectStorageEmulator(storage, "127.0.0.1", 9199);
->>>>>>> 92204dfd913a2c855d40c941b8a51efb051042a7
->>>>>>> Stashed changes
 }
 
 export default app;
