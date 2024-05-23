96d0ebb9c51ec3b2bcd26040751eb82eb36971c2
Raffles Teh Weihan <71363661+Drako-san@users.noreply.github.com>
2024-05-23 17:32:38 +1000

    Button in expand group

--- Tribbum/components/tribu/views/groupspage.jsx
+++ Tribbum/components/tribu/views/groupspage.jsx
@@ -3,6 +3,7 @@
 import React, { useState, useEffect } from "react";
 import GroupCard from "../../../components/tribu/elements/groupCard/GroupCard";
 import { getAllData } from "@/firebase/entities/database";
+import Link from "next/link";
 
 const GroupsPage = () => {
   const [groupsData, setGroupsData] = useState([]);
@@ -21,7 +22,7 @@ const GroupsPage = () => {
   }, []);
 
   useEffect(() => {
-    let results = groupsData.filter((group) =>
+    let results = groupsData.filter(group =>
       group.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
@@ -38,22 +39,30 @@ const GroupsPage = () => {
 
   return (
     <div className="container mx-auto pt-16 md:pt-20 px-4 flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-2xl font-bold mb-4 text-center">All Groups</h1>
-      <div className="search-and-sort mb-4 w-full max-w-md mx-auto">
-        <input
-          type="text"
-          placeholder="Search groups..."
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          className="p-2 border rounded border-gray-300 w-full mb-2"
-        />
-        <button
-          onClick={toggleSort}
-          className="p-2 bg-blue-500 text-white rounded hover:bg-blue-600 w-full"
-        >
-          {sortAsc ? "Sort Z-A" : "Sort A-Z"}
+    <h1 className="text-2xl font-bold mb-4 text-center">All Groups</h1>
+    <div className="search-and-create w-full max-w-md mx-auto flex items-center space-x-2 mb-4">
+      <input
+        type="text"
+        placeholder="Search groups..."
+        value={searchTerm}
+        onChange={e => setSearchTerm(e.target.value)}
+        className="p-2 border rounded border-gray-300 w-full"
+      />
+      <Link href="create-new-group">
+        <button className="py-2 px-4 text-gray-50 font-medium bg-tertiary-500 hover:bg-secondary-500 rounded-full cursor-pointer active:scale-95">
+          +
         </button>
-      </div>
+      </Link>
+    </div>
+    <div className="sort-button w-full max-w-md mx-auto mb-4">
+      <button
+        onClick={toggleSort}
+        className="p-2 bg-blue-500 text-white rounded hover:bg-blue-600 w-full"
+      >
+        {sortAsc ? "Sort Z-A" : "Sort A-Z"}
+      </button>
+    </div>  
+
       <div className="w-full flex flex-wrap justify-center">
         {filteredGroups.map((group, index) => (
           <div key={index} className="m-4" style={{ maxWidth: "340px" }}>
